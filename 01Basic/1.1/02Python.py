fac034555dd35a6d76e541d64fa17f9ab8f3b442
Abhishek Raj <abhishekraj93088@gmail.com>
2026-04-17 16:22:22 +0530

    update

---
+++ 01Basic/1.1/02Python.py
@@ -0,0 +1,14 @@
+# How to Store data in variable
+intValue = 5
+longValue = 7260913389
+StringValue = "Abhishek Kumar Raj"
+floatValue = 8.09
+doubleValue = 5
+charValue = 'a'
+
+print(intValue)
+print(longValue)
+print(StringValue)
+print(floatValue)
+print(doubleValue)
+print(charValue)
---
+++ 01Basic/1.1/03Python.py
@@ -0,0 +1,6 @@
+# Write Program that take Input of the age and print if you are adult or not
+age = int(input("Enter Your Age : "))
+if(age >= 18):
+    print("Your Adult")
+else:
+    print("Your Not Adult")
