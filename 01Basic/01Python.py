5fe636a9dffea3b4f9b71d437d6c228096f07775
Abhishek Raj <abhishekraj93088@gmail.com>
2026-04-17 16:07:17 +0530

    update

---
+++ 01Basic/01Python.py
@@ -0,0 +1,7 @@
+# Things to Know Python or any language
+# Complete the function printNumber which takes an integer input from the user and prints it on the screen.
+# Use:-
+# for Python : print()
+
+number = int(input("Enter Number : "))
+print(number)
