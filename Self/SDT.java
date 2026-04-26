6b3afcc09498dc2e72d0542ad897e612eb9bd055
Abhishek Raj <abhishekraj93088@gmail.com>
2026-04-26 15:09:42 +0530

    update

--- Self/SDT.java
+++ Self/SDT.java
@@ -60,29 +60,69 @@
 
 
 //  sum of the digit
+// import java.util.Scanner;
+
+// public class SDT{
+
+//     public static int addNum(int num){
+//         int temp = num;
+//         int add = 0;
+//         while(temp > 0){
+//             int rem = temp % 10;
+//             add += rem;
+//             temp /= 10;
+//         }
+//         return add;
+//     }
+
+//     public static void main(String[] args){
+//         Scanner sc = new Scanner(System.in);
+//         int t = sc.nextInt();
+//         while(t>0){
+//             int num = sc.nextInt();
+//             System.out.println(addNum(num));
+//         }
+//         sc.close();
+//     }
+// }
+
+
+// Neon Number
 import java.util.Scanner;
 
 public class SDT{
 
-    public static int addNum(int num){
-        int temp = num;
-        int add = 0;
-        while(temp > 0){
-            int rem = temp % 10;
-            add += rem;
-            temp /= 10;
+    public static boolean neonNum(int num){
+        int org = num;
+        int sq = num * num;
+        int fin = 0;
+        while(sq > 0){
+            int rem = sq % 10;
+            fin += rem;
+            sq /= 10;
         }
-        return add;
+        return org == fin;
     }
-
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
+        System.out.print("Enter No of times : ");
         int t = sc.nextInt();
-        while(t>0){
+        
+        while (t > 0) {
             int num = sc.nextInt();
-            System.out.println(addNum(num));
+            if(neonNum(num)){
+                System.out.println("Neon "+ num);
+            }
+            else{
+                System.out.println("Not a Neon "+ num);
+            }
+            t--;
         }
         sc.close();
     }
 }
+
+
+
+
 
