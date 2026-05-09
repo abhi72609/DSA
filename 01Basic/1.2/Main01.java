3e4f0a68ab2ded76a5b36c581c7350641945161d
Abhishek Raj <abhishekraj93088@gmail.com>
2026-05-09 22:27:29 +0530

    update

---
+++ 01Basic/1.2/Main01.java
@@ -0,0 +1,28 @@
+// Tome Complexity
+// public class Main01 {
+//     // TC - O(N²)
+//     public static void main(String[] args) {
+//         int n = 5;
+
+//         for(int i = 0; i < n; i++) {
+//             for(int j = 0; j < n; j++) {
+//                 System.out.println("Hello");
+//             }
+//         }
+//     }
+// }
+
+
+public class Main01 {
+    // TC - O(N²) || // TC - O(N² / 2)
+    public static void main(String[] args) {
+        int n = 6;
+
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < i; j++) {
+                System.out.print("Hello ");
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ 01Basic/1.2/MainPattern02.java
@@ -0,0 +1,11 @@
+//      Patterns Problem
+public class MainPattern02 {
+    public static void main(String[] args) {
+        for(int i=1;i<=4;i++){
+            for(int j=1;j<=4;j++){
+                System.out.print(" * ");
+            }
+            System.out.println();
+        }
+    }
+}
