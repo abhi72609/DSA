1b09d8024bd9f1e3faebb52539ab86fa862ff4e7
Abhishek Raj <abhishekraj93088@gmail.com>
2026-05-12 16:00:35 +0530

    update

--- 01Basic/1.2/MainPattern02.java
+++ 01Basic/1.2/MainPattern02.java
@@ -130,31 +130,144 @@ import java.util.Scanner;
 
 
 
+//       6
+//       * * * * * * * * *
+//         * * * * * * *
+//           * * * * *
+//             * * *
+//               *
+// public class MainPattern02 {
+//     public static void pattern(int n){
+//         int st = (n*2)-1;
+//         int sp = 0;
+//         for(int i=1;i<=n;i++){ // row
+//             for(int j=1;j<=sp;j++){ // For Space
+//                 System.out.print("  ");
+//             }
+//             for(int k=1;k<=st;k++){ // For Star
+//                 System.out.print(" *");
+//             }
+//             st-=2;
+//             sp+=1;
+//             System.out.println();
+//         }
+        
+//     }
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         System.out.print("Enter no of rows : ");
+//         int n = sc.nextInt();
+//         pattern(n);
+//         sc.close(); 
+//     }
+// }
+
+
+
+//      7
+//        *
+//      * * *
+//    * * * * *
+//  * * * * * * *
+//    * * * * *
+//      * * *
+//        *
+
+// public class MainPattern02 {
+//     public static void pattern(int n){
+//         int st = 1;
+//         int sp = n/2;
+
+//         for(int i=1; i<=n; i++){
+
+//             for(int j=1; j<=sp; j++){
+//                 System.out.print("  ");
+//             }
+
+//             for(int k=1; k<=st; k++){
+//                 System.out.print(" *");
+//             }
+
+//             if(i <= n/2){
+//                 st += 2;
+//                 sp -= 1;
+//             }
+//             else{
+//                 st -= 2;
+//                 sp += 1;
+//             }
+//             System.out.println();
+//         }
+//     }
+
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         System.out.print("Enter no of rows: ");
+//         int n = sc.nextInt();
+//         pattern(n);
+//         sc.close();
+//     }
+// }
+
+
+// 8
+//  *    
+//  * *  
+//  * * *
+//  * *  
+// //  *  
+// public class MainPattern02 {
+//     public static void pattern(int n){
+//         int st = 1;
+//         int sp = n/2;
+
+//         for(int i=1; i<=n; i++){
+//             for(int k=1; k<=st; k++){
+//                 System.out.print(" *");
+//             }
+//             for(int j=1; j<=sp; j++){
+//                 System.out.print("  ");
+//             }
+
+//             if(i <= n/2){
+//                 st += 1;
+//                 sp -= 1;
+//             }
+//             else{
+//                 st -= 1;
+//                 sp += 1;
+//             }
+//             System.out.println();
+//         }
+//     }
+
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         System.out.print("Enter no of rows: ");
+//         int n = sc.nextInt();
+//         pattern(n);
+//         sc.close();
+//     }
+// }
+
+
+
+
+
+// 9 
 
 public class MainPattern02 {
     public static void pattern(int n){
-        int st = 1;
-        for(int i=1;i<=n;i++){ // row
-            int sp = n-i;
-            for(int j=1;j<=sp;j++){ // For Space
-                System.out.print("  ");
-            }
-            for(int k=1;k<=st;k++){ // For Star
-                System.out.print(" *");
-            }
-            st+=2;
-            System.out.println();
-        }
         
     }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter no of rows : ");
+        System.out.print("Enter no of rows: ");
         int n = sc.nextInt();
         pattern(n);
         sc.close();
     }
+
 }
-
-
 
