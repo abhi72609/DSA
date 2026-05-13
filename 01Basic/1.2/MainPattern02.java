7fe70d31408c71d38aa992d740dd71b055f94e99
Abhishek Raj <abhishekraj93088@gmail.com>
2026-05-13 22:25:30 +0530

    update

--- 01Basic/1.2/MainPattern02.java
+++ 01Basic/1.2/MainPattern02.java
@@ -260,67 +260,55 @@ import java.util.Scanner;
 //  1  0  1 
 //  0  1  0  1 
 //  1  0  1  0  1 
+// public class MainPattern02 {
+//     public static void pattern(int n){
+//         for(int i=1;i<=n;i++){
+//             if(i % 2 == 0){
+//                 for(int j=1;j<=i;j++){
+//                     if(j % 2 == 0){
+//                         System.out.print(" 1 ");
+//                     }
+//                     else{
+//                         System.out.print(" 0 ");
+//                     }
+//                 }
+//             }
+//             else{
+//                 for(int j=1;j<=i;j++){
+//                     if(j % 2 == 0){
+//                         System.out.print(" 0 ");
+//                     }
+//                     else{
+//                         System.out.print(" 1 ");
+//                     }
+//                 }
+//             }
+//             System.out.println();
+//         }
+//     }
+
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         System.out.print("Enter no of rows: ");
+//         int n = sc.nextInt();
+//         pattern(n);
+//         sc.close();
+//     }
+
+// }
+
+
+
+
 public class MainPattern02 {
     public static void pattern(int n){
         for(int i=1;i<=n;i++){
-            if(i % 2 == 0){
-                for(int j=1;j<=i;j++){
-                    if(j % 2 == 0){
-                        System.out.print(" 1 ");
-                    }
-                    else{
-                        System.out.print(" 0 ");
-                    }
+            for(int j=1;j<=i;j++){
+                if((i+j) % 2 == 0){
+                    System.out.print(" 1 ");
                 }
-            }
-            else{
-                for(int j=1;j<=i;j++){
-                    if(j % 2 == 0){
-                        System.out.print(" 0 ");
-                    }
-                    else{
-                        System.out.print(" 1 ");
-                    }
-                }
-            }
-            System.out.println();
-        }
-    }
-
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter no of rows: ");
-        int n = sc.nextInt();
-        pattern(n);
-        sc.close();
-    }
-
-}
-
-
-
-
-public class MainPattern02 {
-    public static void pattern(int n){
-        for(int i=1;i<=n;i++){
-            if(i % 2 == 0){
-                for(int j=1;j<=i;j++){
-                    if(j % 2 == 0){
-                        System.out.print(" 1 ");
-                    }
-                    else{
-                        System.out.print(" 0 ");
-                    }
-                }
-            }
-            else{
-                for(int j=1;j<=i;j++){
-                    if(j % 2 == 0){
-                        System.out.print(" 0 ");
-                    }
-                    else{
-                        System.out.print(" 1 ");
-                    }
+                else{
+                    System.out.print(" 0 ");
                 }
             }
             System.out.println();
