4ebe79430f1522b7baca262d7bce1cd13b03db78
Abhishek Raj <abhishekraj93088@gmail.com>
2026-07-01 18:59:08 +0530

    update

---
+++ 01Basic/1.3/02CountDigit.py
@@ -0,0 +1,17 @@
+# number = int(input("Enter The Number : "))
+# dupl = number
+# count = 0
+# while (dupl > 0):
+#     count += 1
+#     dupl //= 10
+# print(count)
+
+#       OPTIMIZE CODE
+# cnt = int(log10(number)+1)
+# print(cnt)
+
+
+# for i in range(3):
+#     for j in range(3):
+#         print(i,j) 
+
