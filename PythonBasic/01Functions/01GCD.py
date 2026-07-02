9961ede6874a2048babd99a799f638687c6bd306
Abhishek Raj <abhishekraj93088@gmail.com>
2026-07-02 10:39:43 +0530

    update

---
+++ PythonBasic/01Functions/01GCD.py
@@ -0,0 +1,10 @@
+#       GCD
+
+# Approach 1
+a = int(input("ENter 1st Number : "))
+b = int(input("ENter 2st Number : "))
+x = min(a,b)
+for i in range(x,0,-1):
+    if a % i == 0 and b % i == 0:
+        print("GCD of A and B is :",i)
+        break
