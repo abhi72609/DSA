1dc4d56f803a3b3bab5d4e3ffc245e28d9b2eabd
Abhishek Raj <abhishekraj93088@gmail.com>
2026-07-07 16:21:23 +0530

    updated

--- PythonBasic/02List/02MedList.py
+++ PythonBasic/02List/02MedList.py
@@ -19,3 +19,40 @@ new_runs = []
 for i in range(1,6):
     new_runs.append(runs[-i])
 print(new_runs)
+
+
+print(runs[::1])
+print(runs[::-1])
+
+print(runs[::2])
+print(runs[::-2])
+
+print(runs[-1:-6:-1])
+
+
+
+# 1ST APPROACH
+numbers = [1,2,3,4,5]
+new_list = []
+
+for i in range(0,len(numbers),1):
+    if i < len(numbers)-1:
+        new_list.append(numbers[i])
+    else :
+        new_list.insert(0,numbers[i])
+
+print(new_list)
+
+# 2ND APPROACH
+print([numbers[-1]]+numbers[0:4])
+
+
+
+# list Reversal
+new_value = [1,2,3,4,5]
+new_value.reverse()
+print(new_value)
+
+y = [1,2,3,4,5]
+print(list(reversed(y)))
+print(y)
